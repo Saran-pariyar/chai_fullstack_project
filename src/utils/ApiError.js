383dbaa1ac1ad5130116b2b0649617b709e66f03
Saran Pariyar <spariyar05@gmail.com>
2024-03-01 18:44:31 +0545

    Util code - Custom api response & error handling

---
+++ src/utils/ApiError.js
@@ -0,0 +1,29 @@
+// we create error message class so that we can send errors message more better way
+
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        statck = ""
+    ) {
+        //now we can change these datas and give use this to show error messages next time
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false; //it's false because this is error message
+        this.errors = errors
+
+        //this below is optional, understanding it is better but
+
+        if(statck){
+            this.stack = statck
+        }
+        else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export {ApiError}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+            .catch((err) => next(err))
+    }
+}
+
+export { asyncHandler }
+
+// THIS IS TRY CATCH WAY. WE USE HIGH ORDER FUNCTION TOO HERE
+/*
+const asyncHandler = (func) => {
+    async (req, res, next) => {
+
+        try {
+            await func(req, res, next)
+        }
+        catch (error) {
+            res.status(eror.code || 500).json({
+                success: false,
+                message: error.message
+            })
+        }
+    }
+}
+
+*/ 
