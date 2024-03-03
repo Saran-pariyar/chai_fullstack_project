7171f9c87f0509c5d371d3d9d5f307a43d989e82
Saran Pariyar <spariyar05@gmail.com>
2024-03-03 22:04:57 +0545

    Added JWT - GenerateAccessToken & generateRefreshToken methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,20 +53,51 @@ const userSchema = new Schema({
 
 // it is a mongoose middleware to run just before sending data to db. See more Middleware types in mongoose docs
 // we cannot use arrow function here as it may cause error. It may take time to encrypt it so we make it async
-userSchema.pre("save", async function (next){
-    
-    //we check and run this only if password is modified or else it will change password hash with everytime with this middleware
-    if(this.isModified("password")) return next();
+userSchema.pre("save", async function (next) {
 
-        this.password=bcrypt.hash(this.password, 10)
-   
+    //we check and run this only if password is modified or else it will change password hash with everytime with this middleware
+    if (this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10)
+
 
     next()
 })
 
 //this method checks the password
-userSchema.methods.isPasswordCorrect = async function (password){
+userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password);
 }
+
+userSchema.methods.generateAccessToken = function () {
+
+    jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName
+    },
+
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () { 
+
+    jwt.sign({
+        _id: this._id,
+     
+    },
+
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 
 export const User = mongoose.model("User", userSchema)
