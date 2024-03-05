a9c725a703bb63cf0a6fa5584150d33fee5e5e22
Saran Pariyar <spariyar05@gmail.com>
2024-03-05 22:14:43 +0545

    setting up multer basic

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,21 @@
+import multer from "multer";
+
+//copy pasting this code template from multer github docs
+const storage = multer.diskStorage({
+
+//cb means callback
+    destination: function (req, file, cb) {
+        //that second parameter is location of where we will keep the file
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname)
+    }
+  })
+  
+export const upload = multer({ 
+    storage, 
+})
+
+// learn more about multer from it's docs
