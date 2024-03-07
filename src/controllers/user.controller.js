3ec5432b9ccea354c5db2924b47c323b200b4328
Saran Pariyar <spariyar05@gmail.com>
2024-03-07 22:18:29 +0545

    Getting data from form and writing Algo to register user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,36 @@ const registerUser  = asyncHandler(async(req,res)=>{
    res.status(200).json({
     message: "user registered"
    })
+
+
+
+/*
+ALGORITHM FOR REGISTER USER
+
+Steps:
+
+- Get user details from frontend
+- validation - not empty
+- check if user already exists: username, email
+- check for images, check for avatar
+- upload them to cloudinary, avatar
+- create user object - create entry in db
+- remove password and refresh token field from response
+- check for user creation
+- return response
+
+*/
+
+// we need to get data first. We get these data from request's body from the form when they send these data
+
+const {fullName, email, username, password} = req.body
+console.log("email: ", email);
+
+
 })
 
+
+
+
 export {registerUser}
+
